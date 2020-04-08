ca3451d7283989613bb499d09e765fb7ad003d8e
Mishania13 <mishk777@yandex.ru>
2020-04-08 19:07:50 +0300

    Initial Commit

---
+++ httpwithRequest/Model/URLS.swift
@@ -0,0 +1,16 @@
+//
+//  URLS.swift
+//  httpwithRequest
+//
+//  Created by Mr. Bear on 08.04.2020.
+//  Copyright © 2020 Mr. Bear. All rights reserved.
+//
+
+import Foundation
+
+struct URLS {
+    let imageURL = "https://static-eu.insales.ru/files/1/6033/10958737/original/Asketes.jpg"
+    let getURL = "https://jsonplaceholder.typicode.com/posts"
+    let postURL = ""
+
+}
---
+++ httpwithRequest/ViewControlers/ImageVC.swift
@@ -0,0 +1,43 @@
+//
+//  ImageVC.swift
+//  httpwithRequest
+//
+//  Created by Mr. Bear on 08.04.2020.
+//  Copyright © 2020 Mr. Bear. All rights reserved.
+//
+
+import UIKit
+
+class ImageVC: UIViewController {
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activitiIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activitiIndicator.isHidden = true
+        activitiIndicator.hidesWhenStopped = true
+        fetchImage()
+    }
+    
+    func fetchImage() {
+        
+        activitiIndicator.isHidden = false
+        activitiIndicator.startAnimating()
+        let urls = URLS()
+        guard let url = URL(string: urls.imageURL) else {return}
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activitiIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        } .resume()
+    }
+}
+
